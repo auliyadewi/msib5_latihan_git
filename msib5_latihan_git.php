6a4d5b6221328149c2f2fa591c229c930f848cf0
auliyadewi <139748451+auliyadewi@users.noreply.github.com>
2023-08-22 15:24:08 +0700

    first commit

---
+++ msib5_latihan_git.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampilan Web Biodata</title>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+    <header>
+        <h1>Biodata Auliya Dewi Safitri</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Biodata</h2>
+            <p>Nama: <?php echo "Auliya Dewi Safitri"; ?></p>
+            <p>Nama Unniversitas: <?php echo "Universitas Pembangunan Nasional Veteran Jakarta"; ?></p>
+            <p>Asal Universitas: <?php echo "Jakarta Selatan"; ?></p>
+            <p>Fakultas: <?php echo "Ilmu Komputer"; ?></p>
+            <p>Jurusan: <?php echo "Sistem Informasi"; ?></p>
+        </section>
+        <section>
+            <h2>Kesan dan Pesan Singkat Mengikuti MSIB 5 GITS Studi Independen</h2>
+            <ul>
+               <?php echo "Sangat senang sekali karena bisa belajar di luar kampus. Dengan waktu yang Fleksibilitas tidak menganggu aktivitas saya lainnya dan saya dapat mengatur jadwal belajar saya sendiri. Tentu juga dengan para mentor dan tenaga pengajar yang sangat asik dan ramah dan Saya juga sangat senanng diberi kesempatan belajar di GITS dan semoga dengan mengikuti GITS Studi Independen 5 saya mendapatkan banyak ilmu dan mendapatkan pengalaman yang menarik.  "; ?>
+            </ul>
+        </section>
+    </main>
+</body>
+</html>
